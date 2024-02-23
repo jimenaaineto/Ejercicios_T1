ee55b4e93526905956efd0abddd097ed206dfd38
jimenaaineto <159237388+jimenaaineto@users.noreply.github.com>
2024-02-23 18:43:43 +0000

    ejercicio6

--- ejercicios/Ejercicio_5.java
+++ ejercicios/Ejercicio_5.java
@@ -1,22 +1,26 @@
 package ejercicios;
 import java.util.Scanner;
+/*Escribe un programa que visualice por pantalla la tabla de
+multiplicar de los 10 primeros números naturales. Utilizar una función que
+reciba un número N y devuelva un array de arrays con las tablas de
+multiplicar de 0 a 9 de los N números */
 public class Ejercicio_5 {
     public static void main(String[] args) {
         int num;
         System.out.println("introduzca  un numero: ");
         Scanner leer=new Scanner(System.in);
         num=leer.nextInt();
-        int []todasTablas=tablasNum(num);
+        int [][]todasTablas=tablasNum(num);
 
     }
 
-    public static int []tablasNum (int N){
-        int[]tablas=new int[100];
+    public static int [][]tablasNum (int N){
+        int[][]tablas=new int[10][100];
         for (int j=1;j<=N;j++){
             System.out.println("la tabla del "+j+" es:");
             for(int i=1;i<=10;i++){
-            tablas[i]=i*j;
-            System.out.println(j+"*"+i+"="+tablas[i]);
+            tablas[j][i]=i*j;
+            System.out.println(j+"*"+i+"="+tablas[j][i]);
         }
     }
         
---
+++ ejercicios/Ejercicio_6.java
@@ -0,0 +1,35 @@
+package ejercicios;
+
+import java.util.Scanner;;
+
+public class Ejercicio_6 {
+    public static void main(String[] args) {
+        int N;
+        System.out.println("introduzca un numero: ");
+        Scanner leer=new Scanner(System.in);
+        N=leer.nextInt();
+        int []listaPrimos=numerosPrimos(N);
+
+    }
+
+    public static int []numerosPrimos(int N){
+        int[]lista=new int [N];
+        int cont=0;
+        System.out.println("la lista de numeros primos de los "+N+" primeros numeros es:\n1\n2\n3");
+        for(int i=4;i<=N;i++){
+            if (i==5||i==7||i==11||i==13){
+                System.out.println(i);
+            } else if (i%2==0||i%3==0){
+                continue;
+            } else if (i%5==0||i%7==0) {
+                continue;
+
+            }else {
+                System.out.println(i);
+                lista[cont]=i;
+                cont++;
+            }
+        }
+        return lista;
+    }
+}
