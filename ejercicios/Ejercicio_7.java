9932a388c3e5b05aeb05f4c13713349cc5beca77
jimenaaineto <159237388+jimenaaineto@users.noreply.github.com>
2024-02-27 13:02:06 +0000

    nuevo intento

--- ejercicios/Ejercicio_7.java
+++ ejercicios/Ejercicio_7.java
@@ -3,7 +3,7 @@ package ejercicios;
 guarde en una variable, lo envíe a una función que lo descomponga en
 factores primos, y dicha función devuelva un array que contenga los factores
 primos. Posteriormente imprima el array en el main. ; por ejemplo 40 = 2 * 2
-* 2 * 5. */
+* 2 * 5.*/
 
 import java.util.Scanner;
 
